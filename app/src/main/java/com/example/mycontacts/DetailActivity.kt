84f15ecca17f99e1bd4039fad02646f5accf3f21
arthu <arthurguilhermesb@gmail.com>
2022-02-12 18:51:45 -0300

    create Detail activity

--- app/src/main/java/com/example/mycontacts/DetailActivity.kt
+++ app/src/main/java/com/example/mycontacts/DetailActivity.kt
@@ -1,11 +1,11 @@
 package com.example.mycontacts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class ContactDetail : AppCompatActivity() {
+class DetailActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_detail)
+        setContentView(R.layout.contact_detail)
     }
 }
